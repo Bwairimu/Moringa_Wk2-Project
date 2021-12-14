bb100bca8b1563ae8b03c36a2a4a03370cdbdeae
Bah Wairimu <bah.irimu@gmail.com>
2021-12-14 08:42:45 +0300

    Add js changes

--- js/names.js
+++ js/names.js
@@ -8,11 +8,35 @@ function returnFLDayOfTheWeek(x) {
     return (x < 1) || (x > 7) ? null : flnames[x];
 }
 function namesFunction() {
-    var dateofbirth=document.getElementById("dateofbirth").value;
-    var date=new Date(dateofbirth); // Or your date here
-    var date=date.getDate();
-    var month=date.getMonth()+1;
-    var year=date.getFullYear();
-    window.alert("dateofbirth");
-    
+    var genderValue;
+    if (document.getElementById("malegender").checked) {
+        genderValue = document.getElementById("malegender").value;
+    } else if (document.getElementById("femalegender")) {
+        genderValue = document.getElementById("femalegender").value;
+    }
+    var dateofbirth = document.getElementById("dateofbirth").value;
+    var date = new Date(dateofbirth); //Enter your date here
+    var date = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+    if (date <= 0 || date > 31) {
+        window.alert("Invalid date,please input a correct date");
+    } else if (month <= 0 || month > 12) {
+        window.alert("Invalid month,please input a correct month");
+    } else {
+        var yy = parseInt(year.toString().substr(-2));
+        var cc = parseInt(year.toString().slice(0,2));
+        var dayoftheweek = (((cc/ 4) - 2 * cc- 1) + ((5 * yy / 4)) + ((26 * (month + 1) / 10)) + day) % 7;
+        if (genderValue == "Male") {
+            returnMlDayOfWeek(dayoftheweek);
+        } else if (genderValue == "Female") {
+            returnFlDayOfWeek(dayoftheweek);
+            if (genderValue == "male") {
+                window.alert(returnMlDayOfWeek(dayoftheweek));
+            } else if (genderValue == "female") {
+                window.alert(returnFlDayOfWeek(dayoftheweek));
+            }
+        }
+    }
+
 }
