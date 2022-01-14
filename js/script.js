f4b2326a70fdbe55382e9e90d493ee232f9730d4
Bah Wairimu <bah.irimu@gmail.com>
2022-01-14 22:32:11 +0300

    Added js scripts

--- js/script.js
+++ js/script.js
@@ -1,24 +1,57 @@
-
-let mlname = {Sunday: 'Kwasi', Monday: 'Kwadwo', Tuesday: 'Kwabena', Wednesday: 'Kwaku',
-  Thursday: 'Yaw', Friday: 'Kofi', Saturday: 'Kwame'};
-let flname = {Sunday: 'Akosua', Monday: 'Adwoa', Tuesday: 'Abenaa',
-  Wednesday: 'Akua', Thursday: 'Yaa', Friday: 'Afua', Saturday: 'Ama'};
- 
-var dayoftheweek = (((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (month + 1) / 10)) + day) % 7;
-function checkInputs(){
-  let dateofbirth=dateofbirth.value.trim();
-  let mlname=mlname.value.trim();
-  let flname=flname.value.trim();
-  let date = new Date(dateofbirth); //Enter your date here
-}
-if (date <= 0 || date > 31) {
-  window.alert("Invalid date,please input a correct date");
+var dateOfBirth;
+dateOfBirth = window.prompt("Enter your birthday (YYYY-MM-DD)?", " ");
+var birthdayArray = dateOfBirth.split('-');
+var day = birthdayArray[2];
+var month = birthdayArray[1];
+var year = birthdayArray[0];
+//validation
+if (birthdayArray.length !== 3) {
+  alert("Invalid date, please input a correct date");
+} else if (day <= 0 || day > 31) {
+  alert("Invalid day, please input a correct day");
 } else if (month <= 0 || month > 12) {
-  window.alert("Invalid month,please input a correct month");
+  alert("Invalid month, please input a correct month");
 } else {
-  let yy=parseInt(year.toString().substr(-2));
-  let cc=parseInt(year.toString().slice(0, 2));
-
-  let result=(dateofbirth,genderValue);
-  alert("Your Akan Name is: ");
-  
+  var century = year.slice(0, 2);
+  console.log(century);
+  var bornyear = year.slice(2, 5);
+  console.log(bornyear);
+  getDayOfWeek(century,bornyear,month,day);
+}
+function getDayOfWeek(century,bornyear,month,day){
+  var CC = parseInt(century);
+  var YY = parseInt(bornyear);
+  var MM = parseInt(month);
+  var DD = parseInt(day);
+  var dayOfWeek = (((CC/4)-2*CC-1) + ((5*YY/4)) + ((26*(MM+1)/10)) + DD ) % 7;
+  var dw = Math.floor(dayOfWeek);
+  console.log(dw);
+  akanNames(dw,gender);
+};
+function akanNames(dw) {
+  switch (dw){
+    case 1:
+      alert("You were born on Sunday, your akan name is Kwasi");
+      break;
+    case 2:
+      alert("You were born on Monday, your akan name is Kwadwo");
+      break;
+    case 3:
+      alert("You were born on Tuesday, your akan name is Kwabena");
+      break;
+    case 4:
+      alert("You were born on Wednesday, your akan name is Kwaku");
+      break;
+    case 5:
+      alert("You were born on Thursday, your akan name is Yaw");
+      break;
+    case 6:
+      alert("You were born on Friday, your akan name is Kofi");
+      break;
+    case 7:
+      alert("You were born on Saturday, your akan name is Kwame");
+      break;
+    default:
+      alert("Oops! Try Again.");
+  };
+};
